5b3bd7824e4df4010e99407f6a4660ad1ca1360b
RomainWirth <wirth.romain@gmail.com>
2021-08-25 18:57:16 +0200

    début template app

--- frontend/groupomania/src/App.vue
+++ frontend/groupomania/src/App.vue
@@ -1,6 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <section class="en-tete">
+      <div>
+        <img alt="Vue logo" src="./assets/logo.png">
+        <h1>GROUPOMANIA</h1>
+      </div>
+      <nav class="navigation">
+        <p class="log-in">log-in</p>
+        <p class="forum">forum</p>
+        <p class="conversation">conversation</p>
+      </nav>
+    </section>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -24,5 +34,20 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  width: 960px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.en-tete {
+  display: flex;
+  justify-content: space-around;
+}
+
+.navigation {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
 }
 </style>
