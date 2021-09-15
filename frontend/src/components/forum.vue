e6352c4b01f5129e9ffed4a675cd6b4305c6f83a
RomainWirth <wirth.romain@gmail.com>
2021-09-15 14:01:45 +0200

    réinstall npm, express et axios + modif forum.vue

--- frontend/src/components/forum.vue
+++ frontend/src/components/forum.vue
@@ -1,25 +1,11 @@
 <template>
   <div class="forum">
-    <h1 class="forum__title">{{ msg }}</h1>
+    <h1 class="forum__title">Bienvenue sur le forum de discussion de Groupomania</h1>
     <div class="forum__new-topic">
       <button type="submit">Nouveau</button>
     </div>
-    <div class="forum__topics"> <!-- boucle v-for selon le nombre de sujets créés -->
-      <h2 class="forum__topics--title">Titre topic</h2> <!-- cliquable : donne accès au topic en question -->
-      <div class="forum__topics--details">
-        <p>absdefghijklmnopqrstuvwxyz</p>
-        <p>10/10/2010 00:00:00</p>
-      </div>
-    </div>
-    <div class="forum__topics"> <!-- boucle v-for selon le nombre de sujets créés -->
-      <h2 class="forum__topics--title">Titre topic</h2> <!-- cliquable : donne accès au topic en question -->
-      <div class="forum__topics--details">
-        <p>absdefghijklmnopqrstuvwxyz</p>
-        <p>10/10/2010 00:00:00</p>
-      </div>
-    </div>
-    <div class="forum__topics"> <!-- boucle v-for selon le nombre de sujets créés -->
-      <h2 class="forum__topics--title">Titre topic</h2> <!-- cliquable : donne accès au topic en question -->
+    <div class="forum__topics" v-for="topic_message in topic_messages" :key="topic_message.tm_id"> <!-- boucle v-for selon le nombre de sujets créés -->
+      <h2 class="forum__topics--title">{{ topic_message.tm_titre }}</h2> <!-- cliquable : donne accès au topic en question -->
       <div class="forum__topics--details">
         <p>absdefghijklmnopqrstuvwxyz</p>
         <p>10/10/2010 00:00:00</p>
@@ -32,12 +18,32 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: 'forum',
-  props: {
-    msg: String
-  }
-}
+  data() {
+    return {
+      topic_messages: [],
+    };
+  },
+  created() {
+    this.showTopicMessages();
+  },
+  methods: {
+    // get all messages
+    async showTopicMessages() {
+      try {
+        const response = await axios.get("http://localhost:/5000/topic_messages");
+        this.topic_messages = response.data;
+      } catch (err) {
+        console.log(err);
+      }
+    },
+    // 
+
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
